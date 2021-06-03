1cbb91dcb1bbd847f521966dfd85a2ced414e4ce
khlnn <khlnn@DESKTOP-J34PDV0>
2021-06-03 22:06:50 +0530

    uploading created project

---
+++ CholaprojectinSpringBoot/src/main/java/com/javapoint/CholaprojectinSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.javapoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CholaprojectinSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CholaprojectinSpringBootApplication.class, args);
+	}
+
+}
